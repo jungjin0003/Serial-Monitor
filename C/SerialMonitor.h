7737d1474d94a490fc48f10aeefe9637f349f4ed
jungjin0003 <jungjin00031@gmail.com>
2022-01-25 16:40:25 +0900

    update SerialMonitor.h

--- C/SerialMonitor.h
+++ C/SerialMonitor.h
@@ -7,3 +7,5 @@ HANDLE GetCommHandleByComNumber(DWORD ComNumber);
 BOOL SetBaudRate(HANDLE hComm, DWORD BaudRate);
 DWORD SerialWrite(HANDLE hComm, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite);
 DWORD SerialRead(HANDLE hComm, LPVOID lpBuffer, DWORD nNumberOfBytesToRead);
+
+CRITICAL_SECTION ConsoleLock;
